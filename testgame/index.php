ebfab195c3ef4ea393240e69cc83c652f3568d58
phong1912 <49642898+phong1912@users.noreply.github.com>
2021-04-03 13:43:40 +0700

    web game

---
+++ testgame/index.php
@@ -0,0 +1,26 @@
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="webgame.aspx.cs" Inherits="DoAnGame.webgame" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="draw.js"></script>
+</head>
+    <body onload="draw(cellStartup);">
+        
+
+    <div class="board" id="board">
+                    <img id="scream" src="images/Cboard.jpg" alt="Board">
+                    <canvas id="canvas" id="canvas" width="540" height="600"></canvas>
+                    <!--<span class="cursor" id="cursor"></span>-->
+                    <div class="alert" id="alert" style="display: none">
+                        <span class="w-l" id="w-l">You win</span>
+                        <span class="continue" id="continue" onclick="reset()">Continue</span>
+                    </div>
+        <h1>huy</h1>
+        <h2></h2>
+                </div>
+   
+</body>
+    
